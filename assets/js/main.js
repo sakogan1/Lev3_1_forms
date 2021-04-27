a225ea4966e670920b3fa35102a2ef85572e9a84
sakogan1 <samir.kogan11@gmail.com>
2021-04-27 17:04:23 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+function changeSize(){
+    var ran = document.getElementById("range").value
+    var px ="px"
+    var size = ran + px
+    console.log(size)
+    document.getElementById("areaToChange").style.fontSize = size
+}
+
+function changeFont(){
+    var font = document.getElementById("font-select").value
+    console.log(font)
+    document.getElementById("areaToChange").style.fontFamily = font
+}
+function writeText(){
+    var textValue =document.getElementById("internaltext").value
+    document.getElementById("areaToChange").innerHTML = textValue
+}
+
